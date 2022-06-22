3c7c9b82527034f7a1638098aca36487315af4d0
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-22 12:15:23 +0530

    Added : Code testing using JUnit for each test for sad and happy

--- src/user/UserValidationTest.java
+++ src/user/UserValidationTest.java
@@ -13,28 +13,63 @@ public class UserValidationTest {
 	public void setUp() {
 		valid=new UserValidation();
 	}
+ 
 	@Test
-	public void firstNameTest() {
+	public void firstNameTesthappy() {
+//		for happy test case
 		boolean isValid=valid.firstNameValidation("Asjad");
 		Assert.assertEquals(true, isValid);		
 	}
 	@Test
-	public void lastNameTest() {
+	public void firstNameTestsad() {
+//		for sad test case
+		boolean isValid=valid.firstNameValidation("asjad");
+		Assert.assertEquals(false, isValid);		
+	}
+	
+	@Test
+	public void lastNameTestHappy() {
+//		happy test case
 		boolean isValid=valid.lastNameValidation("Azmi");
 		Assert.assertEquals(true, isValid);
 	}
 	@Test
-	public void emailTest() {
+	public void lastNameTestSad() {
+//		sad test case
+		boolean isValid=valid.lastNameValidation("azmi");
+		Assert.assertEquals(false, isValid);
+	}
+	@Test
+	public void emailTestHappy() {
+//		happy test case
 		boolean isValid=valid.emailValidation("xyz.abc@bl.co.in");
 		Assert.assertEquals(true, isValid);
 	}
+	public void emailTestSad() {
+//		sad test case
+		boolean isValid=valid.emailValidation("xyz.abcbl.co.in");
+		Assert.assertEquals(false, isValid);
+	}
 	@Test
-	public void mobileTest() {
+	public void mobileTestHappy() {
+//		Happy test case
 		boolean isValid=valid.mobileValidation("918748835815");
 		Assert.assertEquals(true, isValid);
 	}
+	public void mobileTestSad() {
+//		Sad test case
+		boolean isValid=valid.mobileValidation("910048835815");
+		Assert.assertEquals(false, isValid);
+	}
 	@Test
-	public void passwordTest() {
+	public void passwordTestHappy() {
+//		Happy test case
+		boolean isValid=valid.passwordValidation("ASj@#$>/");
+		Assert.assertEquals(true, isValid);
+	}
+	@Test
+	public void passwordTestSad() {
+//		Sad test case
 		boolean isValid=valid.passwordValidation("ASj@#$>/");
 		Assert.assertEquals(true, isValid);
 	}
